f73d30c520e4b2c0d76cdb8e45e3dd773594a71d
mehdi <146713129+alouane-mehdi@users.noreply.github.com>
2024-01-30 18:07:34 +0100

    Add files via upload

---
+++ jour1/job7.sql
@@ -0,0 +1,12 @@
+mysql> INSERT INTO etudiant (prenom, nom, email, age)
+    -> VALUES
+    ->     ('Betty', 'Spaghetti', 'betty.Spaghetti@laplateforme.io', 23),
+    ->     ('Chuck', 'Steak', 'chuck.steak@laplateforme.io', 45),
+    ->     ('John', 'Doe', 'john.doe@laplateforme.io', 18),
+    ->     ('Binkie', 'Barnes', 'binkie.barnes@laplateforme.io', 16),
+    ->     ('Gertrude', 'Dupuis', 'gertrude.dupuis@laplateforme.io', 20);
+Query OK, 5 rows affected (0.01 sec)
+Records: 5  Duplicates: 0  Warnings: 0
+
+
+(pas réussi à mettre en fichier sql via terminal..)
